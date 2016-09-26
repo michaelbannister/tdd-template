32ae6ad051f7d816b222e71fca24aa95b64f11ad
Michael Bannister <github@michaelbannister.co.uk>
2016-09-26 20:55:27 +0100

    Convert test to Spek

---
+++ src/test/kotlin/DecimalToRomanConverterSpec.kt
@@ -0,0 +1,55 @@
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+class DecimalToRomanConverterSpec : Spek({
+    describe("Int.toRoman extension method") {
+        arrayOf(
+            Pair(1, "I"),
+            Pair(2, "II"),
+            Pair(10, "X"),
+            Pair(20, "XX"),
+            Pair(100, "C"),
+            Pair(200, "CC"),
+            Pair(1000, "M"),
+            Pair(2000, "MM"),
+            Pair(11, "XI"),
+            Pair(3012, "MMMXII"), // I didn't expect this to fail, just checking
+            Pair(5, "V"),
+            Pair(50, "L"),
+            Pair(500, "D"),
+            Pair(1555, "MDLV"), // again, just checking
+            Pair(4, "IV"),
+            Pair(444, "CDXLIV"), // again, just checking
+            Pair(666, "DCLXVI"),
+            Pair(777, "DCCLXXVII"),
+            Pair(888, "DCCCLXXXVIII"),
+            Pair(999, "CMXCIX"),
+            Pair(1888, "MDCCCLXXXVIII") // longest Roman numeral year
+        ).forEach {
+            val (number, roman) = it
+            it("should convert ${number} to ${roman}") {
+                assertEquals(roman, number.toRoman())
+            }
+        }
+    }
+    describe("toDecimalPlaceValues function") {
+        arrayOf(
+            Pair(9, "9*1"),
+            Pair(14, "1*10 + 4*1"),
+            Pair(4025, "4*1000 + 2*10 + 5*1"),
+            Pair(10, "1*10")
+        ).forEach {
+            val (number, placevalueString) = it
+            it("should split ${number} into place values ${placevalueString}") {
+                assertEquals(placevalueString, formatPlaceValues(toDecimalPlaceValues(number)))
+            }
+        }
+
+    }
+})
+
+private fun formatPlaceValues(placeValues: List<PlaceValue>) = placeValues.joinToString(separator = " + ")
+
+
